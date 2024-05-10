bf0c9b89ef5305f7f34bca59cb877423c074ac1f
rastadayon <rastadayon@gmail.com>
2024-05-10 14:52:22 -0700

    Change save_model function

--- src/train.py
+++ src/train.py
@@ -60,10 +60,13 @@ def write_model(model, path):
     torch.save(model.state_dict(), path)
     print("Model Saved")
 
-def save_model(model, dir_path, model_name):
-    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    os.makedirs(dir_path, exist_ok=True)
-    filename = f"{dir_path}/{model_name}_{current_time}.pth"
+def save_model(model, dir_path, model_name, with_time=False):
+    if with_time:
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/{model_name}_{current_time}.pth"
+    else:
+        filename = f"{dir_path}/{model_name}.pth"
     print("Writing Model")
     torch.save(model, filename)
     print("Model Saved")
