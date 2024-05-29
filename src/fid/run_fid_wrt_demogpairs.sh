4bf7302a790f2f59894e3f39dfd6e9e170b45bdd
rastadayon <rastadayon@gmail.com>
2024-05-28 22:53:08 -0700

    Add fid vs bias plots

--- src/fid/run_fid_wrt_demogpairs.sh
+++ src/fid/run_fid_wrt_demogpairs.sh
@@ -1,8 +1,8 @@
-python3 fid/fid_score.py 
+python3 fid/fid_score.py \
 --clean_dataset demogpairs \
 --batch-size 128 \
 --device cuda:2 \
 --dims 2048 \
 --save-stats \
---save-stats-dir ./debug/demogpairs \
---results-path ./debug/demogpairs/fid.json
+--save-stats-dir /media/global_data/fair_neural_compression_data/decoded_rfw/decoded_64x64/fid_data/demogpairs \
+--results-path /media/global_data/fair_neural_compression_data/decoded_rfw/decoded_64x64/fid_data/demogpairs/fid.json
