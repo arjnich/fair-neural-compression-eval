9b7bd72b9b61f269b997a1f02ad0c711bae3fd46
rastadayon <rastadayon@gmail.com>
2024-05-30 17:25:57 -0700

    Change the rfw_numerical.py to sort labels

--- src/rfw_numerical_label.py
+++ src/rfw_numerical_label.py
@@ -20,29 +20,40 @@ def one_hot_to_numerical(rfw_df):
     rfw_df['hair_color'] = rfw_df[hair_color_columns].idxmax(axis=1)
     rfw_df = rfw_df.drop(columns=hair_color_columns)
     
+    unique_skin_types = list(rfw_df['skin_type'].unique())
+    unique_skin_types.sort()
     rfw_df['skin_type'] = rfw_df['skin_type'].replace(
-        list(rfw_df['skin_type'].unique()),
-        range(len(list(rfw_df['skin_type'].unique())))
+        list(unique_skin_types),
+        range(len(list(unique_skin_types)))
     )
+    unique_lip_types = list(rfw_df['lip_type'].unique())
+    unique_lip_types.sort()
     rfw_df['lip_type'] = rfw_df['lip_type'].replace(
-            list(rfw_df['lip_type'].unique()),
-            range(len(list(rfw_df['lip_type'].unique())))
+            unique_lip_types,
+            range(len(unique_lip_types))
     )
+    unqiue_nose_types = list(rfw_df['nose_type'].unique())
+    unqiue_nose_types.sort()
     rfw_df['nose_type'] = rfw_df['nose_type'].replace(
-            list(rfw_df['nose_type'].unique()),
-            range(len(list(rfw_df['nose_type'].unique())))
+            unqiue_nose_types,
+            range(len(unqiue_nose_types))
     )
+    unique_eye_types = list(rfw_df['eye_type'].unique())
+    unique_eye_types.sort()
     rfw_df['eye_type'] = rfw_df['eye_type'].replace(
-            list(rfw_df['eye_type'].unique()),
-            range(len(list(rfw_df['eye_type'].unique())))
+            unique_eye_types,
+            range(len(unique_eye_types))
     )
+    unique_hair_types = list(rfw_df['hair_type'].unique())
+    unique_hair_types.sort()
     rfw_df['hair_type'] = rfw_df['hair_type'].replace(
-            list(rfw_df['hair_type'].unique()),
-            range(len(list(rfw_df['hair_type'].unique())))
+            unique_hair_types,
+            range(len(unique_hair_types))
     )
+    unique_hair_colors = list(rfw_df['hair_color'].unique())
     rfw_df['hair_color'] = rfw_df['hair_color'].replace(
-            list(rfw_df['hair_color'].unique()),
-            range(len(list(rfw_df['hair_color'].unique())))
+            unique_hair_colors,
+            range(len(unique_hair_colors))
     )
     return rfw_df
 
@@ -50,4 +61,4 @@ if __name__ == "__main__":
     rfw_df = pd.read_csv('/media/global_data/fair_neural_compression_data/datasets/RFW/clean_metadata/clean.csv')
     rfw_df = one_hot_to_numerical(rfw_df)
     
-    rfw_df.to_csv('/media/global_data/fair_neural_compression_data/datasets/RFW/clean_metadata/numerical_labels.csv', index=False)
+    rfw_df.to_csv('/media/global_data/fair_neural_compression_data/datasets/RFW/clean_metadata/numerical_labels_sorted.csv', index=False)
